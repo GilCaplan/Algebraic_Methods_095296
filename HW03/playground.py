c6d1264eea5407a121fbb84d743d1c48676679fa
GilCaplan <rockycaplan@gmail.com>
2024-02-20 12:47:26 +0200

    wrote code for QR division of a matrix
    added solution for part 2,3 of Q8

--- HW03/playground.py
+++ HW03/playground.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 
-
+#Q8
 def QR_division(A):
     z, Q = Grand_Shit(A)
     R = find_R(A, Q, z)
@@ -60,7 +60,8 @@ def QR_division(A):
 
 def find_R(A, Q, Z):
     R = np.array([[0 for _ in range(len(A[0]))] for _ in range(len(A[0]))])
-
+    Q = np.array(Q)
+    A = np.array(A)
     for i in range(len(R)):  # row
         for j in range(len(R[0])):  # col
             if i == j:
@@ -71,7 +72,7 @@ def find_R(A, Q, Z):
 
 
 def Grand_Shit(A):
-    A = A.T
+    A = np.array(A).T
     z = np.array([[0 for _ in range(len(A[0]))] for _ in range(len(A))], float)
     z[0] = A[0]
 
@@ -101,5 +102,32 @@ def normalize(V):
 Mat = np.array([[1, -1, 4], [1, 4, -2], [1, 4, 2], [1, -1, 0]])
 Q, R = QR_division(Mat)
 
-print(Q)
-print(R)
+
+
+M = [[3, 6, 8, 0, 4, 3, 1, 5, 4, 4],
+ [4 ,0 ,6 ,5 ,1 ,9 ,3 ,3 ,3 ,3],
+ [5 ,0 ,9 ,8 ,0 ,4 ,9 ,6 ,6 ,4],
+ [0 ,7 ,6 ,9 ,2 ,5 ,5 ,5 ,3 ,4],
+ [2 ,3 ,8 ,1 ,2 ,2 ,6 ,6 ,6 ,4],
+ [5 ,4 ,1 ,8 ,1 ,5 ,8 ,9 ,5 ,3],
+ [0 ,1 ,7 ,5 ,3 ,7 ,9 ,4 ,0 ,7],
+ [2 ,9 ,2 ,8 ,3 ,4 ,8 ,2 ,2 ,5],
+ [6 ,6 ,0 ,0 ,4 ,6 ,8 ,2 ,7 ,1],
+ [4 ,7 ,8 ,6 ,4 ,8 ,7 ,8 ,2 ,7],
+ [7 ,5 ,9 ,9 ,5 ,1 ,8 ,4 ,3 ,8],
+ [2 ,4 ,9 ,2 ,9 ,4 ,0 ,7 ,0 ,8],
+ [2 ,8 ,2 ,4 ,2 ,4 ,6 ,3 ,5 ,1],
+ [2 ,9 ,6 ,8 ,2 ,5 ,9 ,0 ,0 ,9],
+ [1 ,4 ,5 ,2 ,2 ,2 ,2 ,6 ,9 ,5]]
+
+x = np.array([21,11,9,6,5,4,2,1,94,91,89,85,84,16,98])
+
+np.set_printoptions(precision=2)
+Q1, R1 = QR_division(M)
+print(Q1)
+print(R1)
+print("part 2:", Q1 @ Q1.T @ x)
+# projection of x on S is equal to P @ P^T @ x
+print("part 3", (np.eye(len(Q1)) - Q1 @ Q1.T) @ x)
+# projection of x on S complement is (I - P @ P^T) @ x as proved in HW02 QB
+
